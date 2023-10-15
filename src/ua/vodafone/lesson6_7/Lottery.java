469cb3a11e8d1f6aea28da980f8e6613bdb33857
Oleksandr Shevchenko <shallbe@gmail.com>
2023-10-15 20:09:24 +0300

    Fixed comparing last item of array.

--- src/ua/vodafone/lesson6_7/Lottery.java
+++ src/ua/vodafone/lesson6_7/Lottery.java
@@ -47,7 +47,7 @@ public class Lottery {
     }
 
     private static void sortArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     swap(arr, j, j + 1);
